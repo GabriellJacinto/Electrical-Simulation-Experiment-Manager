c59506930078a90764418f0c090b1cc291179265
GabrielLJacinto <gabriel.jacinto@pixforce.ai>
2023-06-11 12:38:55 -0300

    major refactor to support spectre files

--- main.py
+++ main.py
@@ -2,19 +2,20 @@ import argparse
 
 from src.ExperimentManager import ExperimentManager
 from src.CSVmanager import CSVmanager
-import src.HspiceRunner as HspiceRunner
+import SpiceRunner as SpiceRunner
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Spice Simulation Manager")
     parser.add_argument('-o', dest='out_dir', required=True, help="Complete output path for results")
     parser.add_argument('-p', dest='parameters', required=True, help="Json file containing two dictionaties: config (configurations for the spice script and its path) and var (values that will be permutated during simulation)")
+    parser.add_argument('-w', dest='max_workers', required=False, help="Maximum number of parallel simulations", default=6)
 
     args = parser.parse_args()
 
-    experiment = ExperimentManager(args, HspiceRunner.run)
-    experiment.run(max_workers=6)
+    experiment = ExperimentManager(args, SpiceRunner.run)
+    experiment.run(max_workers=args.max_workers)
     
     if experiment.command == "hspice": 
         CSVmanager.join_all_hspice(experiment.out_dir)
     elif experiment.command == "spectre":
-        CSVmanager.join_all(experiment.out_dir)
+        CSVmanager.join_all_spctre(experiment.out_dir, experiment.input_file_name)
